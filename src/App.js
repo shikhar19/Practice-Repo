63286f98583fe15424affcf554cfb8d0ffae24c5
Shikhar Sen <Shikhar.Sen@shl.com>
2024-11-02 13:52:09 +0000

    Program to Implement a Stopwatch/timmer as shown below with start ,stop and reset button

--- src/App.js
+++ src/App.js
@@ -1,19 +1,49 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 
 function App(){
-  const[isVisible, setIsVisible] = useState(false);
-  const setVisibility = ()=>{
-    setIsVisible(!isVisible);
+  const [seconds, setSeconds] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+  const timerRef = useRef(null);
+
+  const startTimer = () => {
+    if(!isActive){
+      setIsActive(true);
+      timerRef.current = setInterval(()=>{
+        setSeconds((prevSec) => prevSec +1);
+      }, 1000);
+    }
   };
+
+  const stopTimer = () =>{
+    if(isActive){
+      clearInterval(timerRef.current);
+      setIsActive(false);
+    }
+  };
+
+  const resetTimer = () =>{
+    clearInterval(timerRef.current);
+    setIsActive(false);
+    setSeconds(0);
+  };
+
+  useEffect (() =>{
+    return ()=> clearInterval(timerRef.current);
+  }, [])
+
+  const minutes = Math.floor(seconds/60);
+  const secs= seconds%60;
 
   return(
     <div style = {{textAlign : 'center', marginTop : '50px'}}>
-      <h1>Simple Visibilty check</h1>
-      
-      <button onClick={setVisibility}>{isVisible ? 'Hide Text' : 'Show Text'}</button>
-      {
-      isVisible && <p>This is the text content that can be shown or hidden</p>
-    }
+      <h1>Timer</h1>
+      <span>{minutes} mins </span>
+      <span>{secs} secs </span>
+      <div>
+        <button onClick={startTimer}> start</button>
+        <button onClick={stopTimer} >stop</button>
+        <button onClick={resetTimer} >reset</button>
+      </div>
     </div>
     
   );
