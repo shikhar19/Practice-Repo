d778387be6844bf25b7499c99e22e5d9b667865c
Shikhar Sen <Shikhar.Sen@shl.com>
2024-11-02 08:13:53 +0000

    commit changes for react practice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+  const[isVisible, setIsVisible] = useState(false);
+  const setVisibility = ()=>{
+    setIsVisible(!isVisible);
+  };
+
+  return(
+    <div style = {{textAlign : 'center', marginTop : '50px'}}>
+      <h1>Simple Visibilty check</h1>
+      
+      <button onClick={setVisibility}>{isVisible ? 'Hide Text' : 'Show Text'}</button>
+      {
+      isVisible && <p>This is the text content that can be shown or hidden</p>
+    }
     </div>
+    
   );
+  
 }
 
 export default App;
